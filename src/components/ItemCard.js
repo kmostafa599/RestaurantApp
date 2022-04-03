a78b413e834ec2a8afe051863a7910324f64dcf1
Kareem Mostafa <kmostafa599@gmail.com>
2022-04-03 15:15:50 +0200

    Commit-1

---
+++ src/components/ItemCard.js
@@ -0,0 +1,51 @@
+import { Button, useTheme } from '@mui/material';
+import React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+// import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+// import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+// import SkipNextIcon from '@mui/icons-material/SkipNext';
+import seafood from '../icons/Seafood.png'
+const ItemCard = () => {
+    const theme = useTheme();
+
+    return (
+        <div>
+            <Card sx={{ display: 'flex', boxShadow:"none" }}>
+            <CardMedia
+                    component="img"
+                    sx={{ width: 151 }}
+                    image={seafood}
+                    alt="Live from space album cover"
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <CardContent sx={{ flex: '1 0 auto' }}>
+                        <Typography component="div" variant="h5">
+                            Seafood
+                        </Typography>
+                        <Typography variant="subtitle1" color="text.secondary" component="div">
+                            Shrimp,
+                        </Typography>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                        <IconButton aria-label="previous">
+                            {theme.direction === 'rtl' ? '+' : '-'}
+                        </IconButton>
+                        <IconButton aria-label="play/pause">
+                            1          </IconButton>
+                        <IconButton aria-label="next">
+                            {theme.direction === 'rtl' ? '-' : '+'}
+                        </IconButton>
+                    </Box>
+                </Box>
+                
+            </Card>
+        </div>
+    )
+}
+
+export default ItemCard
