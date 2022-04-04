3754e025819147c7a3c70c4a774793741c6fc6e5
Kareem Mostafa <kmostafa599@gmail.com>
2022-04-04 05:24:34 +0200

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import { Stack } from '@mui/material';
+import React, { useState } from 'react';
 import './App.css';
 import Banner from './components/Banner';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { makeStyles } from '@material-ui/core'
+import ItemCard from './components/ItemCard';
+
+
 
 function App() {
+  const [counter,setCounter] = useState()
+  
+  const classes = useStyles() 
+  
+  
   return (
     <div className="App">
       <Stack spacing={3}>
         <Header />
         <Banner/>
-        <Menu />
+        <Menu counter={counter} setCounter={setCounter}/>
       </Stack>
 
-
+      <div>
+        
+          
     </div>
+    </div>
+
   );
 }
 
