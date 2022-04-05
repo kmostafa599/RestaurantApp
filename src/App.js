ad95aa303dff7b37429aea5e1faac272cf562928
Kareem Mostafa <kmostafa599@gmail.com>
2022-04-05 11:01:05 +0200

    Admin

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { makeStyles } from '@material-ui/core'
 import ItemCard from './components/ItemCard';
+import Cart from './components/Cart';
+import Test from './components/Test';
+import Success from './components/Success';
+import Dashboard from './components/Dashboard';
+import AdminHeader from './components/AdminHeader';
 
 
 
@@ -28,8 +33,13 @@ function App() {
     <div className="App">
       <Stack spacing={3}>
         <Header />
-        <Banner/>
-        <Menu counter={counter} setCounter={setCounter}/>
+        {/* <AdminHeader style={{}}/> */}
+         <Banner/>
+        <Menu counter={counter} setCounter={setCounter}/> 
+        {/* <Cart/> */}
+        {/* <Test/> */}
+        {/* <Success/> */}
+        <Dashboard/>
       </Stack>
 
       <div>
